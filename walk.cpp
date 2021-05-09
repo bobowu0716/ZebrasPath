8445bcace87f1ff918d7dd3dd3d1414533c54fdf
bobowu0716 <>
2021-05-09 11:55:57 +0000

    Adding comment

--- walk.cpp
+++ walk.cpp
@@ -19,14 +19,17 @@ int main(){
   ZebrasPath book(startingrow,startingcolumn);
   book.print();
   while (endgame==false){
+    //Ask the player to enter a move.
     cout<<"Make a move (row col): "
     cin<<zebrar<<zebrac;
     book.move(zebrar,zebrac);
+    //Check the move of the zebra the player entered is valid or not. If not then warn and ask the player to enter a new one.
     if(book.move(zebrar,zebrac)==false){
       cout<<"Invalid move. Try again!\n";
     }
     else{
       book.print();
+      //To check the zebra has more moves or not.If no, then end the game.
       book.hasMoreMoves();
       if(book.hasMoreMoves==false){
         cout<<"No more moves!\n;
