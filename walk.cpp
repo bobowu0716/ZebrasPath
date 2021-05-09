bd4df9e9795b83e444768efbcdeb896c447a8769
bobowu0716 <>
2021-05-09 11:52:30 +0000

    Finished

--- walk.cpp
+++ walk.cpp
@@ -3,11 +3,35 @@
 using namespace std;
 
 int main(){
-  int startingrow,startingcolumn;
+  int startingrow,startingcolumn,zebrar,zebrac;
   bool startgame=false;
+  bool endgame==false;
   while(startgame==false){
     cout<<"Zebra's starting position (row col): ";
     cin>>startingrow>>startingcolumn;
-    
+    if (startingrow>=0&&startingrow<=7&&startingcolumn>=0&&startingcolumn<=7){
+      startgame=true;
+    }
+    else{
+      cout<<"Invalid. Try again!\n";
+    }
+  }
+  ZebrasPath book(startingrow,startingcolumn);
+  book.print();
+  while (endgame==false){
+    cout<<"Make a move (row col): "
+    cin<<zebrar<<zebrac;
+    book.move(zebrar,zebrac);
+    if(book.move(zebrar,zebrac)==false){
+      cout<<"Invalid move. Try again!\n";
+    }
+    else{
+      book.print();
+      book.hasMoreMoves();
+      if(book.hasMoreMoves==false){
+        cout<<"No more moves!\n;
+        endgame=true;
+      }
+    }
   }
 }
